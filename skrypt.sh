0dda7e45d766021b053ec3af9e0bb9c7411e5a3a
Kubson <jakub.kucharski@opoczta.plgit config --global user.name ,,Jakub>
2023-04-05 16:52:31 +0200

    Zadanie5

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ $1 == "--date" ]]; then
-  echo $(date)
+if [[ "$1" == "--date" ]]; then
+    date "+%Y-%m-%d"
 elif [[ "$1" == "--logs" ]]; then
     if [[ -z "$2" ]]; then
         for i in {1..100}; do
@@ -12,4 +12,11 @@ elif [[ "$1" == "--logs" ]]; then
             echo "log$i.txt created by $0 on $(date)" > "log$i.txt"
         done
     fi
+elif [[ "$1" == "--help" ]]; then
+    echo "Możliwe opcje do wykonania:"
+    echo "--date: Wyswietla date"
+    echo "--logs: Tworzy pliki log z numerami od 1 do 100"
+    echo "--logs: Tworzy pliki log z numerami od 1 do liczby podanej przy uruchamianiu skryptu"
+else
+    echo "Nie ma takiej opcji"
 fi
