e16a2c2b8d514fa3add3c166fd2b5718c51285a8
Kubson <jakub.kucharski@opoczta.plgit config --global user.name ,,Jakub>
2023-04-05 16:50:00 +0200

    Zadanie4

--- skrypt.sh
+++ skrypt.sh
@@ -7,5 +7,9 @@ elif [[ "$1" == "--logs" ]]; then
         for i in {1..100}; do
             echo "log$i.txt created by $0 on $(date)" > "log$i.txt"
         done
+    else
+        for i in $(seq 1 $2); do
+            echo "log$i.txt created by $0 on $(date)" > "log$i.txt"
+        done
     fi
 fi
